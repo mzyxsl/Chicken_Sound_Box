2e25090487eec65e5163e52f7bfa4f37f6bc0b79
名字已消失了 <48387476+mzyxsl@users.noreply.github.com>
2025-08-12 12:28:51 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mzyxsl.cxk"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
